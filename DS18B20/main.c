1cdf6bf64d63c4c89e2264c9941c87ceb5b2adcc
Rickx137 <rickfromc137@qq.com>
2026-08-13 12:12:07 +0800

    加入设备检测报警，并优化设备使用时被拔出的显示,并加入注释

--- DS18B20/main.c
+++ DS18B20/main.c
@@ -17,32 +17,33 @@ void main()
 	unsigned char K;
 	LCD1602_Init();
 	Timer0_Init();
-	THigh = AT24C02_ReciveByte(0);
-	TLow = (signed char)AT24C02_ReciveByte(1);
-	if(THigh > 125 || TLow < -55 || THigh <= TLow)
+	THigh = AT24C02_ReciveByte(0);//上电读取eeprom里的数据
+	TLow = (signed char)AT24C02_ReciveByte(1);//转化成有符号的
+	if(THigh > 125 || TLow < -55 || THigh <= TLow)//防止第一次读取错误数据，初始化
 	{
 		THigh = 35;
 		TLow = 18;
 		AT24C02_WriteByte(0,THigh);
-		Delay(5);
+		Delay(5);//留出时间写
 		AT24C02_WriteByte(1,TLow);
 		Delay(5);
 	}
 	//DS18B20_BitSet(10);//设置为10bit，第一次烧录后可注释掉，已存入eeprom里
 	while(1)
 	{
-		if(DS18B20_Start() == 1)//测量温度
+		if(DS18B20_Start() == 1)//检测设备是否存在
 		{
-			LCD1602_ShowString(1,1,"Err   ");
+			LCD1602_ShowString(1,1,"Err             ");//清屏并显示错误
 			LCD1602_ShowString(2,1,"                ");
+			Buzzer_Time(150);//警报
 		}
 		else
 		{
 			K = KEY();
 			LCD1602_ShowString(1,1,"T:");
-			LCD1602_ShowString(2,7,"TL:");
+			LCD1602_ShowString(2,8,"TL:");
 			LCD1602_ShowString(2,1,"TH:");
-			if(K)
+			if(K)//按键控制阈值，并逻辑判断，保存
 			{
 				if(K == 1)
 				{
@@ -67,6 +68,10 @@ void main()
 					{
 						TLow--;
 					}
+					else if(TLow == 0)
+					{
+						LCD1602_ShowChar(2,13,' ');//防止在==0的时候有一位仍保留
+					}
 				}
 				else if(K == 4)
 				{
@@ -75,20 +80,24 @@ void main()
 					{
 						TLow = -55;
 					}
+					else if(TLow == 0)
+					{
+						LCD1602_ShowChar(2,13,' ');
+					}
 				}
 				AT24C02_WriteByte(0,THigh);
 				Delay(5);
 				AT24C02_WriteByte(1,TLow);
 				Delay(5);
 			}
-			LCD1602_ShowSignedNum(2,4,THigh,2);
-			LCD1602_ShowSignedNum(2,10,TLow,2);
+			LCD1602_ShowSignedNum(2,4,THigh,3);
+			LCD1602_ShowSignedNum(2,11,TLow,2);
 			while(!OneWire_ReceiveBit());//测温时返回0，完成返回1，以此判断
 			T = DS18B20_ReadT();
 			if(T < 0)
 			{
 				LCD1602_ShowChar(1,3,'-');
-				TD = -T;
+				TD = -T;//使现实的翻转，用原始数据与阈值比大小
 			}
 			else
 			{
@@ -100,17 +109,17 @@ void main()
 			LCD1602_ShowNum(1,7,(unsigned long)(TD*100)%100,2);//显示小数部分
 			if(T >= THigh)
 			{
-				LCD1602_ShowString(1,11,"H!");
-				Buzzer_Time(500);
+				LCD1602_ShowString(1,11,"H!");//高温预警
+				Buzzer_Time(200);
 			}
 			else if(T <= TLow)
 			{
-				LCD1602_ShowString(1,11,"L!");
-				Buzzer_Time(500);
+				LCD1602_ShowString(1,11,"L!");//低温预警
+				Buzzer_Time(200);
 			}
 			else if(T < THigh && T > TLow)
 			{
-				LCD1602_ShowString(1,11,"  ");
+				LCD1602_ShowString(1,11,"  ");//清除预警
 			}
 
 		}
@@ -119,7 +128,7 @@ void main()
 }
 
 
-void Timer0() interrupt 1
+void Timer0() interrupt 1//检测按键
 {
 	static unsigned int Counts = 0;
 	TL0 = 0x18;
